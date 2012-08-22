23cf0486a8dedb16898d5e1aa794165d46ce11fe
Alessandro Molina <alessandro.molina@axant.it>
2012-08-22 14:25:18 +0200

    Raise version for bugfix release

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ except IOError:
 
 setup(
     name='tgapp-calendarevents',
-    version='0.0.2',
+    version='0.0.3',
     description='TurboGears2 pluggable application for events and calendars',
     long_description=README,
     author='Alessandro Molina',
