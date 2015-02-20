82afbf6b8f3804817a60e4d9246499a98d18ab4c
Simone Gasbarroni <simone.gasbarroni@gmail.com>
2015-02-20 05:52:26 +0100

    new utils for getting events filtered

--- calendarevents/lib/utils.py
+++ calendarevents/lib/utils.py
@@ -1,5 +1,7 @@
-import datetime
+from datetime import timedelta
 from calendarevents import model
+from calendarevents.model import CalendarEvent
+
 
 def create_calendar(name, events_type):
     new_calendar = model.Calendar(name=name, events_type=events_type)
@@ -8,15 +10,30 @@ def create_calendar(name, events_type):
 
 
 def create_event(cal, name, summary, datetime, location, linked_entity_type, linked_entity_id, end_time=None):
-    new_event = model.CalendarEvent(calendar_id=cal.uid, name=name,
-                                    summary=summary, datetime=datetime,
-                                    end_time=end_time,
-                                    location=location,
-                                    linked_entity_type=linked_entity_type,
-                                    linked_entity_id=linked_entity_id)
+    new_event = CalendarEvent(calendar_id=cal.uid, name=name,
+                              summary=summary, datetime=datetime,
+                              end_time=end_time,
+                              location=location,
+                              linked_entity_type=linked_entity_type,
+                              linked_entity_id=linked_entity_id)
     model.DBSession.add(new_event)
     return new_event
 
 
 def get_event(event_id):
+
     return model.DBSession.query(model.CalendarEvent).get(event_id)
+
+
+def get_calendar_events_from_datetime(calendar, datetime):
+
+    return model.DBSession.query(CalendarEvent).filter(CalendarEvent.datetime >= datetime)\
+        .filter(CalendarEvent.calendar_id == calendar)
+
+
+def get_calendar_day_events(calendar, start_time):
+    start_time = start_time.replace(hour=0, minute=0, second=0, microsecond=0)
+    end_time = start_time + timedelta(days=1)
+    return model.DBSession.query(CalendarEvent).filter(CalendarEvent.datetime >= start_time ) \
+            .filter(CalendarEvent.datetime < end_time) \
+            .filter(CalendarEvent.calendar_id == calendar)
